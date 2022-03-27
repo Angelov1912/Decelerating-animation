40df1e3dc8adb1fe34f321c4c5d2acb228ed57d9
Stanimir Angelov <62446471+Angelov1912@users.noreply.github.com>
2022-03-27 22:01:59 +0100

    Add script.js via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function reveal() {
+  //Use queryselector to target the class
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 200;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+//Add event listener on scroll
+window.addEventListener("scroll", reveal);
